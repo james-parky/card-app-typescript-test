3c9891cfd7fd5d929de17972db914a4ce47b8c59
james-parky <72101291+james-parky@users.noreply.github.com>
2024-08-01 13:30:40 +0100

    Moved <EntryContextProvider> to only wrap necessary children. Added a <Header/> containing a title and theme dialog toggle. This is wrapped in a <ThemeContextProvider>.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,29 @@
-import React from "react";
-import NavBar from './components/NavBar'
-import AllEntries from './routes/AllEntries'
-import NewEntry from './routes/NewEntry'
-import EditEntry from './routes/EditEntry'
-import { EntryProvider } from './utilities/globalContext'
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import NavBar from "./components/NavBar";
+import { EntryContextProvider } from "./contexts/EntryContext";
+import { ThemeContextProvider } from "./contexts/ThemeContext";
+import AllEntries from "./routes/AllEntries";
+import EditEntry from "./routes/EditEntry";
+import NewEntry from "./routes/NewEntry";
 
 export default function App() {
   return (
     <section>
-  <Router>
-    <EntryProvider>
-    <NavBar></NavBar>
-      <Routes>
-        <Route path="/" element={<AllEntries/>}>
-        </Route>
-        <Route path="create" element={<NewEntry/>}>
-        </Route>
-        <Route path="edit/:id" element={<EditEntry/>}>
-        </Route>
-      </Routes>
-    </EntryProvider>
-    </Router>
+      <Router>
+        <ThemeContextProvider>
+          <Header />
+        </ThemeContextProvider>
+        <NavBar />
+        <EntryContextProvider>
+          <Routes>
+            <Route path="/" element={<AllEntries />}></Route>
+            <Route path="create" element={<NewEntry />}></Route>
+            <Route path="edit/:id" element={<EditEntry />}></Route>
+          </Routes>
+        </EntryContextProvider>
+      </Router>
     </section>
-    
   );
 }
