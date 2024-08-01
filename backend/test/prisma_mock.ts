7be2f3f84aafae5cb8ec78bcfac60f58b4bcc7aa
james-parky <72101291+james-parky@users.noreply.github.com>
2024-08-01 15:08:20 +0100

    A module containing a jest mock for the prisma client, as per prismas own unit testing guidelines.

---
+++ backend/test/prisma_mock.ts
@@ -0,0 +1,14 @@
+import { DeepMockProxy, mockDeep, mockReset } from "jest-mock-extended";
+import { PrismaClient } from "@prisma/client";
+import Prisma from "../src/db";
+
+jest.mock("../src/db", () => ({
+  __esModule: true,
+  default: mockDeep<PrismaClient>(),
+}));
+
+beforeEach(() => {
+  mockReset(prismaMock);
+});
+
+export const prismaMock = Prisma as unknown as DeepMockProxy<PrismaClient>;
